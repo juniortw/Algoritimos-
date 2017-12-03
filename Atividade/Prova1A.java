5b388d8de4086d617193fe28e5334f31274c6055
Amauri Junior <32683820+juniortw@users.noreply.github.com>
2017-12-03 20:28:00 -0200

    Update Prova1A.java

--- Atividade/Prova1A.java
+++ Atividade/Prova1A.java
@@ -59,7 +59,7 @@ public class Prova1A {
                     System.out.println("Valor Invalido");
                     System.exit(0);
                 }
-                double trapesio = altura*(basemaior + basemenor) / 2;
+                double trapezio = altura*(basemaior + basemenor) / 2;
                 System.out.println("A área do FIGURA é de? "+trapesio);
                 
                 
